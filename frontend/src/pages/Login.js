291587b9e82d31c4372fd1a1e2b3618a833df2b1
Rajan Saini <rajansaini2.0.0.6@gmail.com>
2025-12-11 18:38:55 +0530

    Update login API URL to production endpoint

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -25,7 +25,7 @@ const Login = () => {
     }
 
     try {
-      const url = "http://localhost:8080/auth/login";
+      const url = "https://mern-auth-deploy-api.vercel.app/auth/login";
       const response = await fetch(url, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
